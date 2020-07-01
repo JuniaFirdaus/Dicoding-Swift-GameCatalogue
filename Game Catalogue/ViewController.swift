10b712f04a0d01410caae72c19dfe6503bb49a76
Junia <junia.firdaus@lintasmediadanawa.com>
2020-07-01 22:48:18 +0700

    Initial Commit

---
+++ Game Catalogue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game Catalogue
+//
+//  Created by PT Lintas Media Danawa on 01/07/20.
+//  Copyright © 2020 JFS Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
